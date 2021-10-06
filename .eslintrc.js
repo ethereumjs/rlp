cceea8326440644ed95baeef8c759d494084640d
Ryan Ghods <ryan@ryanio.com>
2021-10-06 18:56:29 +0300

    lint:fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,11 @@ module.exports = {
   extends: '@ethereumjs/eslint-config-defaults',
   parserOptions: {
     project: ['./tsconfig.json'],
-  }
+  },
+  env: {
+    mocha: true,
+  },
+  rules: {
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
 }
